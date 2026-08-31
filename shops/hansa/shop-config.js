03a40552015a970d34bc4feba2a3a369f6e43d98
schustolero <peterlepit1909@gmail.com>
2026-08-31 21:34:57 +0200

    Add files via upload

--- shops/hansa/shop-config.js
+++ shops/hansa/shop-config.js
@@ -6,25 +6,39 @@ window.SHOP_CONFIG = {
   brandSubtitle: "T-Shirt Konfigurator",
   designerHeading: "Shirt gestalten",
   designerIntro: "Motiv auswählen, Farbe bestimmen und Shirt konfigurieren.",
-  accentColor: "#1f3f76",
+
+  // Keine feste Hauptfarbe: neutrales Shop-Design
+  accentColor: "#222222",
+
   logoFile: "shop-logo.jpg",
   logoHeight: 90,
-  shirtPrice: 15, currency: "EUR",
+
+  shirtPrice: 15,
+  currency: "EUR",
   orderEmail: "shirtzentrale@gmail.com",
   orderSubject: "Neue Hansa Berufskolleg T-Shirt Bestellung",
+
   customerExtraFieldLabel: "Klasse",
   customerExtraFieldName: "Klasse",
   orderPrefix: "HAN",
+
   shopType: "motifs",
   features: {
-    layout: "compact", motifMode: "multiple",
-    allowCustomerUpload: false, allowText: false,
-    allowMoveMotif: false, allowResizeMotif: false, allowRotateMotif: false,
-    allowBackDesign: true, allowMotifColor: true,
-    autoSelectSingleMotif: false, maxUploadMB: 8
+    layout: "compact",
+    motifMode: "multiple",
+    allowCustomerUpload: false,
+    allowText: false,
+    allowMoveMotif: false,
+    allowResizeMotif: false,
+    allowRotateMotif: false,
+    allowBackDesign: true,
+    allowMotifColor: true,
+    autoSelectSingleMotif: false,
+    maxUploadMB: 8
   },
+
   motifs: [
-    { id: "college", name: "College", file: "motiv-1.png" },
-    { id: "script", name: "Script", file: "motiv-2.png" }
+    { id: "motiv1", name: "Motiv 1", file: "motiv-1.png" },
+    { id: "motiv2", name: "Motiv 2", file: "motiv-2.png" }
   ]
 };
