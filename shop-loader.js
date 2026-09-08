6f2e1d8d7fdc6ae18c108f6eea2f13f7a46c659c
schustolero <peterlepit1909@gmail.com>
2026-09-08 12:45:53 +0200

    Add files via upload

--- shop-loader.js
+++ shop-loader.js
@@ -17,7 +17,7 @@
 
   function loadFileFallback(callback){
     const script = document.createElement("script");
-    script.src = `/shops/${encodeURIComponent(slug)}/shop-config.js?v=28.5.2`;
+    script.src = `/shops/${encodeURIComponent(slug)}/shop-config.js?v=29.7.5`;
     script.onload = () => callback && callback(window.SHOP_CONFIG || {});
     script.onerror = () => {
       console.error(`Shop-Konfiguration nicht gefunden: ${slug}`);
