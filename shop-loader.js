0d603754e40ed7371b60235340182a689aa03366
schustolero <peterlepit1909@gmail.com>
2026-09-21 13:50:46 +0200

    Add files via upload

--- shop-loader.js
+++ shop-loader.js
@@ -30,7 +30,7 @@
 
   function loadFileFallback(callback){
     const script = document.createElement("script");
-    script.src = `/shops/${encodeURIComponent(slug)}/shop-config.js?v=30.1.93`;
+    script.src = `/shops/${encodeURIComponent(slug)}/shop-config.js?v=30.1.94`;
     script.onload = () => {
       window.SHOP_CONFIG = normalizeTemplateDemo(window.SHOP_CONFIG || {});
       callback && callback(window.SHOP_CONFIG);
