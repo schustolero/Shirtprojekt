271394c192fab146efe39f8a0587f2291954faca
schustolero <peterlepit1909@gmail.com>
2026-09-24 10:10:10 +0200

    Add files via upload

--- shops/_master/shop-config.js
+++ shops/_master/shop-config.js
@@ -1,2 +1,2 @@
-// Master-Shop: zentrale Vorschau des vollständigen Textilsortiments.
+// Master-Shop: einzige Vorlage für neue Kundenshops (Sortiment, Preise, Druck).
 window.SHOP_CONFIG = JSON.parse(JSON.stringify(window.CENTRAL_CONFIG?.seedShops?._master || {}));
